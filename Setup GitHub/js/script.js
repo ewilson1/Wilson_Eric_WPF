81dbac626fe2ae981cb55887427e7232d541f792
ewilson1 <ewilson1@fullsail.edu>
2015-04-08 13:51:46 -0400

    This was my first attempt at js
    I made a mistake and created the file in another folder. In the changes
    to come I will attempt to fix them

--- Setup GitHub/js/script.js
+++ Setup GitHub/js/script.js
@@ -1,13 +1,1 @@
-/*
-Eric Wilson
-Test JS File
-Date
- */
-
-//alert("Testing 1, 2, 3, ");
-
-
-console.log("Testing the console.log");
-
-var userInput = prompt("Type in your name");
-console.log(userInput);
+alert("Hello World ");
