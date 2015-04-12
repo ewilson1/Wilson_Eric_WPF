53fd8600cb0b2c13c4ad5096578017e9916242ac
ewilson1 <ewilson1@fullsail.edu>
2015-04-12 19:47:59 -0400

    Easier Ways to use 'num'
    In this description, the var ‘num’ is used to make it easier to take
    from a string of variables by just placing the line
    
    console.log(muppetNames[num]);

--- Variables/js/script.js
+++ Variables/js/script.js
@@ -3,4 +3,9 @@
 var muppetNames;
 muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
 
-console.log(muppetNames[0]);
+muppetNames[1] = "Rizzo"; // change index #1 to Rizzo
+muppetNames[2] = "Fozzy";
+
+var num = 1; // using the num allows you to change the console.log easier
+
+console.log(muppetNames[num]); //lets me see whats in the index #1
