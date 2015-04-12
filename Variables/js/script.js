fa0b19cd036de55c56bccc54b8809d745cd6743c
ewilson1 <ewilson1@fullsail.edu>
2015-04-12 19:13:29 -0400

    More changes to the js script file
    From the lessons and videos, just some tutorials

--- Variables/js/script.js
+++ Variables/js/script.js
@@ -3,4 +3,10 @@
  */
 
 var age;
-age = 45;
+age = 45; //Number Variable
+
+var isStudent = false; //Boolean Variable
+
+var firstName = "Eric";
+
+var phrase = 'I don\'t now \n You\'ll have to speak with the professor'
