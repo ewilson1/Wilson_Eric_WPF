70b76ceb89f30b82c62860f3803c68071d2e2fca
ewilson1 <ewilson1@fullsail.edu>
2015-04-14 16:57:39 -0400

    Assignment Operations
    This goes over some short cuts, such as =+ and — which mean a + 1 or a
    -1; yet this only works for 1.

---
+++ Expressions - Assignment Operators/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ericwilson1 on 4/14/15.
+ */
+
+//Expressions - Assignment Operators
+
+
+var a = 3;
+//a ++ ;// the same as a = a + 1 // a += 1
+a --;// the same as a = a -1 // a -= 1
+console.log(a);
