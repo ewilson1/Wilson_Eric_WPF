b4a5ee09d16191ef22d52949679edaee0d731900
ewilson1 <ewilson1@fullsail.edu>
2015-04-14 10:41:25 -0400

    Concatenating strings
    In this description I put a space “ “ between my firstName and my
    lastName

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -2,10 +2,12 @@
  * Created by ericwilson1 on 4/13/15.
  */
 
-//Expressions - Expressions with Arrays
+//Expressions - Concatenating Strings
 
-var orangeBins = [234, 567, 883];
 
-var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
-console.log(total);
+var firstName = "Eric";
+var lastName = "Wilson";
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
 
