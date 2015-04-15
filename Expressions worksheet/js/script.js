abe58fae04a23840ed0de9fc0b681ceab3a76bbd
ewilson1 <ewilson1@fullsail.edu>
2015-04-15 11:34:16 -0400

    Dog Years
    This was a bit tricky, I changed the .js file and the index.html file
    to ‘dogYears’ for both. It ran the same with all four up an running.
    However I am curious to see if I delete the original index and .js
    files if it will run the same. Otherwise, I don’t know how we can
    complete the assignment without having a .js script and a html file for
    each problem.

---
+++ Expressions worksheet/js/script.js
@@ -0,0 +1,14 @@
+//Eric Wilson, April 15, 2015 Dog Years//
+
+//Dogs age 7 times faster than Humans, so a dog that is 1 year old in human years is 7 years old in 'dogYears'//
+
+//Sparky is 10 years old in Human years//
+//Sparky's age in dog years is 10 * 7//
+//Sparky's age is 10 human years which is (7 * 10) in dog years//
+
+var a = 10;//Sparky's age in Human years//
+var dogYears = (a * 7);//Sparky's age in Human years,2, is multiplied by 7 to get his age in dog years//
+
+var solution = "Sparky is " + a + " human years old, which is " + dogYears + " in dog years"
+console.log(solution);//This is to print into the console how old Sparky is in dog years//
+
