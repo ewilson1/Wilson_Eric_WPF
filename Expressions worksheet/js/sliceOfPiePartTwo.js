3f92b2f24e47c8346309588d8d647fb9f55fc82b
ewilson1 <ewilson1@fullsail.edu>
2015-04-15 14:55:52 -0400

    sliceOfPiePartOne final had some English changes
    Because I did it backwards, I needed to change the English to match the
    goal of the code.

--- Expressions worksheet/js/sliceOfPiePartTwo.js
+++ Expressions worksheet/js/sliceOfPiePartTwo.js
@@ -1,7 +1,8 @@
 /*
  Eric Wilson, April 15th, 2015 Slice of Pie Part One//
 
- We need to find out how many slices pizza each of the attendees will get; this will be a floating number
+ We need to find out how many slices are left over for Sparky after all the people in the party
+ have had the same amount of slices of pizza.
  */
 
 var people; // declaration, in this case how many people are at the party
@@ -12,4 +13,3 @@ var slices; // declaration; they will be sharing slices of pizza
 slices = 8; // definition; this is the amount of slices per pizza; 1 pizza has 8 slices each
 var sparkySlices = (pizzas * slices) / (people); //this divison operator will give us a floating number
 console.log("Each Person ate " + sparkySlices + " slices of pizza");
-
