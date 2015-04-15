dde959eb37919211141d5320b31c17daf3a93944
ewilson1 <ewilson1@fullsail.edu>
2015-04-15 11:36:07 -0400

    Changed the English to match
    I had the wrong number “2” in where “10” should have been

--- Expressions worksheet/js/dogYears.js
+++ Expressions worksheet/js/dogYears.js
@@ -10,7 +10,7 @@
 //Sparky's age is 10 human years which is (7 * 10) in dog years//
 
 var a = 10;//Sparky's age in Human years//
-var dogYears = (a * 7);//Sparky's age in Human years,2, is multiplied by 7 to get his age in dog years//
+var dogYears = (a * 7);//Sparky's age in Human years, 10 , is multiplied by 7 to get his age in dog years//
 
 var solution = "Sparky is " + a + " human years old, which is " + dogYears + " in dog years"
 console.log(solution);//This is to print into the console how old Sparky is in dog years//
