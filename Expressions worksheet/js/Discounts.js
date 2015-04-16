069b5c392a1f17e7722424fe175297071a821913
ewilson1 <ewilson1@fullsail.edu>
2015-04-16 10:38:46 -0400

    Discount worksheet problem
    using the new code for English working from the top to bottom instead
    of being right of the code.

--- Expressions worksheet/js/Discounts.js
+++ Expressions worksheet/js/Discounts.js
@@ -5,7 +5,17 @@ Discounts
 April 16, 2015
  */
 
-alert("Test to see if connected");
+//alert("Test to see if connected");
 
 
+//Calculate the discounted price for an item. Create an expression that will calculate the discounted price with and without sales tax. There will be a result, more than likely, that's a floating number with decimals
 
+//Given Original price of item, the discount, the description of the item and the sales tax percentage.
+
+// Price of the item
+var shoes = prompt("Let's get you some shoes. \nWhat kind of shoes would you like?");
+console.log(shoes);
+
+// How much did they cost?
+var cost = prompt("How much did your "+shoes+ " cost?");
+console.log(cost);
