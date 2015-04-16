f7d02f40f41588a68d21ba6041328188aedb6703
ewilson1 <ewilson1@fullsail.edu>
2015-04-16 19:46:13 -0400

    Switched the order of the questions
    The order needs to flow smoothly, in this case I changed the dollar
    amount per tank down in the code.

--- Wilson Eric Expressions Assignment/js/Expressions_Assignment.js
+++ Wilson Eric Expressions Assignment/js/Expressions_Assignment.js
@@ -9,15 +9,11 @@
 
 //Brainstorm an idea for a calculator of some kind. Must use an array, 3 prompts for information from the user and use math expressions, et al.
 
-//Givens: (what we will ask the user for) Let's Grill! 1. How much does a tank of fuel cost?, 2. How long does it take to grill chicken, in minutes?, 3. How many chickens, in minutes per chicken, can you grill before the tank runs out? What is the average cost of fuel per chicken?
+//Givens: (what we will ask the user for) Let's Grill! 1. How much does a tank of fuel cost?, 2. How long does it take to grill thier foood, in minutes?, 3. How many chickens, in minutes per chicken, can you grill before the tank runs out? What is the average cost of fuel per chicken?
 
 //Let's ask them what they like to grill.
 var food = prompt("Let's Grill!\nDo you like Chicken or Steak on the grill?");
 console.log("They like " +food+ " on the grill");
-
-//Now let's ask how much they pay to have a full tank of fuel
-var tank = prompt("How much did it cost you to fill up your tank?");
-console.log("It cost them $" +tank+ " dollars to fill up their tank.");
 
 //Now let's ask them how long it takes to cook their food
 var time = prompt("How long does it take to cook your " +food+ " in minutes?");
@@ -27,6 +23,10 @@ console.log("It takes them " +time+ " minutes to cook their food");
 var month = prompt("How many times do you grill your " +food+ " in a month?");
 console.log("They grill " +month+ " times a month.");
 
+//Now let's ask how much they pay to have a full tank of fuel
+var tank = prompt("How much did it cost you to fill up your tank?");
+console.log("It cost them $" +tank+ " dollars to fill up their tank.");
+
 //Time to let them know how many minutes of grilling time they have per unit of fuel
 alert("Each tank holds around 240 minutes of grilling time, so keep that in mind.");
-console.log("Now they are aware of how much time they have in each tank, 240 minutes.");
+console.log("Now they are aware of how much time they have in each tank of fuel, 240 minutes.");
