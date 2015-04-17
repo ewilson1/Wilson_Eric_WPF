2d96c5d2e8225fbe0d4712cccf80de4f535f8413
ewilson1 <ewilson1@fullsail.edu>
2015-04-16 20:20:25 -0400

    Step one is complete, but now let's get harder
    Its time to add a wrinkle to the equation and see how much it costs to
    cook each food of choice

--- Wilson Eric Expressions Assignment/js/Expressions_Assignment.js
+++ Wilson Eric Expressions Assignment/js/Expressions_Assignment.js
@@ -17,11 +17,11 @@ console.log("They like " +food+ " on the grill");
 
 //Now let's ask them how long it takes to cook their food
 var time = prompt("How long does it take to cook your " +food+ " in minutes?");
-console.log("It takes them " +time+ " minutes to cook their food");
+console.log("It takes them " +time+ " minutes to cook their " +food+ ".");
 
 //Now let's find out how many times they grill a month
 var month = prompt("How many times do you grill your " +food+ " in a month?");
-console.log("They grill " +month+ " times a month.");
+console.log("They grill their " +food+ " around " +month+ " times a month.");
 
 //Now let's ask how much they pay to have a full tank of fuel
 var tank = prompt("How much did it cost you to fill up your tank?");
@@ -30,3 +30,9 @@ console.log("It cost them $" +tank+ " dollars to fill up their tank.");
 //Time to let them know how many minutes of grilling time they have per unit of fuel
 alert("Each tank holds around 240 minutes of grilling time, so keep that in mind.");
 console.log("Now they are aware of how much time they have in each tank of fuel, 240 minutes.");
+
+//We have enough information to draw a conclusion as to how many times they can grill before the tank runs out
+var grillTime = (Number(240) / time);
+alert("They can grill " +grillTime+ " " +food+ "'s until the tank runs out.");
+console.log("They can grill " +grillTime+ " " +food+ "'s until the tank runs out.");
+
