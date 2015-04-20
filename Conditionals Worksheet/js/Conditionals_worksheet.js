c1730b854ba636c5529bf3267d6eb1d4267a704f
ewilson1 <ewilson1@fullsail.edu>
2015-04-20 15:19:31 -0400

    Worksheet example one: eating contest
    I set this up the long way and did not use a Ternary.
    I tried both ways, entered in various numbers and they all came up with
    proper results

--- Conditionals Worksheet/js/Conditionals_worksheet.js
+++ Conditionals Worksheet/js/Conditionals_worksheet.js
@@ -10,3 +10,20 @@ April 20th, 2015
  console.log("Test to see if they are connected");
     */
 
+ //To get into the Pie eating contest you must weigh 250 pds or more. Determine if they qualify based on weight.
+
+var weight = 252;
+ var eat;
+
+ /* if(age <10){
+  book = "Green Eggs and Ham";
+  }else{
+  book = "The Time Machine";
+  }*/
+
+ if(weight <= 250){
+  eat = "Gets to enter the pie eating contest, good luck!";
+ }else{
+  eat = "Does not get to eat in the contest";
+ }
+console.log(eat)
