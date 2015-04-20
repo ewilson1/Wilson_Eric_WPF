50521564fb3f610721e88952337af7091e43b76e
ewilson1 <ewilson1@fullsail.edu>
2015-04-20 10:50:56 -0400

    The OR operator ||
    This only needs one value to be true on either side in order to run the
    code. Keep in mind, when declaring a var to be a true or false you are
    using Boolean declaration and don’t need to use ===

--- Conditionals/js/Conditional.js
+++ Conditionals/js/Conditional.js
@@ -8,14 +8,16 @@ Date
  
 //Conditional Logic - Logical Operators
 
-var budget = 300;
+var budget = 100;
  var iPhone = 199.99;
  var payCheck = 200;
+ var wonLottery = true;
 
 
- //if the price of the phone is less than our budget AND if our paycheck is over 300
+ //if the price of the phone is less than our budget OR if we won the Lottery
  //we can only use && when comparing Boolean True or False statements YOU CANT use just numbers between && operator
- if(iPhone < budget && payCheck > 300){
+ //the or operator || only needs ONE of the Boolean values to be true to run the code, unlike the && operator needed both Boolean values needed to be true.
+ if(iPhone < budget || wonLottery){
   console.log("We can buy the phone!");
  }else{
   console.log("No phone for you!");
