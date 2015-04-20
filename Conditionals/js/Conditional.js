7e2f16e5fb4a71e723745947b483a1a805bb6809
ewilson1 <ewilson1@fullsail.edu>
2015-04-20 10:20:51 -0400

    Else if condition
    Here we see what to do with three or more conditions, using the “else
    if” in the second line allows another option for the kid to ride; this
    time with a parent if they are between the other two conditions

--- Conditionals/js/Conditional.js
+++ Conditionals/js/Conditional.js
@@ -6,26 +6,29 @@ Title
 Date
  */
  
-//Conditional Logic -Relational conditions (var examples prove either true or false)
+//Conditional Logic -else if, choosing between three or more blocks of code
 
 //alert("Test to see if they are connected");
 //console.log("Test to see if they are connected");
 
-var kidHeight = 47; //Normally the kid would not be tall enough, but with sneakers which add 2" he is.
+var kidHeight = 46; //Normally the kid would not be tall enough, but with sneakers which add 2" he is.
  var minHeight = 48;
-
+var wParentHeight = 45; // the height of the kid with the parent
 
 //if the child is old enough, print to the console "you can ride"
 //if the kid is over 48 inches in height
 
 
 if(kidHeight > minHeight){
- //code is performed if condition is true
+ //print out you can ride
    console.log ("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+ //you can ride with a parent
+ console.log ("You can ride, but only with a parent present.");
 }else{
- //code is performed if condition is false
+  //print out "sorry you have some growing to do"
  console.log ("Sorry kid, you have some growing to do");
-}
+ }
 
 
 
