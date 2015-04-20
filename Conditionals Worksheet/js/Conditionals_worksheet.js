166a3e8cdfbe256cfa620b8a9ab273627a5a9dde
ewilson1 <ewilson1@fullsail.edu>
2015-04-20 14:04:26 -0400

    Setting up Conditional worksheet
    Tested and they are connected

---
+++ Conditionals Worksheet/js/Conditionals_worksheet.js
@@ -0,0 +1,12 @@
+
+ /*
+Eric Wilson
+SDI Section 01
+Conditionals Worksheet
+April 20th, 2015
+ */
+
+/*alert("Test to see if they are connected");
+ console.log("Test to see if they are connected");
+    */
+
