efc8c65a7b418370dcf5d5dbebdd32bff67e8aec
ewilson1 <ewilson1@fullsail.edu>
2015-04-20 09:44:15 -0400

    Conditionals
    This is the notes regarding conditionals being true and the
    introduction to the {} brackets.

---
+++ Conditionals/js/Conditional.js
@@ -0,0 +1,25 @@
+
+ /*
+Eric Wilson
+SDI Section 01
+Title
+Date
+ */
+ 
+//Conditional Logic
+
+//alert("Test to see if they are connected");
+//console.log("Test to see if they are connected");
+
+var oldEnough = false;
+
+//if the child is old enough, print to the console "you can ride"
+
+if(oldEnough){
+   console.log ("You can ride the coaster!");
+}
+console.log("What comes after the code if the condition is false");
+
+
+
+
