d25d80c1aa28a1be61e3ec8bdf70e4e8206dff60
ewilson1 <ewilson1@fullsail.edu>
2015-04-20 10:43:41 -0400

    Using the && operator
    It is important to note that the && operator is only used to compare
    true/false agains true/false-NOT btw numbers

--- Conditionals/js/Conditional.js
+++ Conditionals/js/Conditional.js
@@ -6,31 +6,17 @@ Title
 Date
  */
  
-//Conditional Logic -else if, choosing between three or more blocks of code
+//Conditional Logic - Logical Operators
 
-//alert("Test to see if they are connected");
-//console.log("Test to see if they are connected");
-
-var kidHeight = 45; //Normally the kid would not be tall enough, but with sneakers which add 2" he is.
- var minHeight = 48;
-var wParentHeight = 45; // the height of the kid with the parent
-
-//if the child is old enough, print to the console "you can ride"
-//if the kid is over 48 inches in height
+var budget = 300;
+ var iPhone = 199.99;
+ var payCheck = 200;
 
 
-if(kidHeight > minHeight){
- //print out you can ride
-   console.log ("You can ride the coaster!");
-}else if(kidHeight >= wParentHeight){
- //you can ride with a parent, notice I put the = in there just in case they are both the same height.
- console.log ("You can ride, but only with a parent present.");
-}else{
-  //print out "sorry you have some growing to do"
- console.log ("Sorry kid, you have some growing to do");
+ //if the price of the phone is less than our budget AND if our paycheck is over 300
+ //we can only use && when comparing Boolean True or False statements YOU CANT use just numbers between && operator
+ if(iPhone < budget && payCheck > 300){
+  console.log("We can buy the phone!");
+ }else{
+  console.log("No phone for you!");
  }
-
-
-
-
-
