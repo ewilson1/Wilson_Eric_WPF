a0b3b3dd96ac797e5615e8e892725f8243dc4516
ewilson1 <ewilson1@fullsail.edu>
2015-04-22 15:43:28 -0400

    Multiple Results...added one more
    This was a test to see if we could use more than one “else if” to keep
    asking questions and getting one result in the console. It works!!!!

--- Conditionals Worksheet/js/Conditionals_worksheet.js
+++ Conditionals Worksheet/js/Conditionals_worksheet.js
@@ -10,14 +10,18 @@ April 20th, 2015
 
  var correctUser = "Eric";
  var correctPass = "Aidan";
+ var secretCode = 1414;
  var welcome = (correctUser && correctPass);
 
  var userInput = prompt("Welcome to our website.\nPlease enter your username.");
- var userPassWord = prompt("Please enter your password");
+ var userPassWord = prompt("Please enter your password.");
+ var userSecretCode = prompt("Please enter your secret code.");
  if(userInput != correctUser) {
    console.log("User name not found. Please try again.");
   }else if(userPassWord != correctPass){
   console.log("Your password does not match our records.");
+ }else if(userSecretCode != secretCode){
+  console.log("Your secret code does not match our records.");
   }else{
   welcome = true;
   console.log("Welcome, " + correctUser + ", to our website!");
