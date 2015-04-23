6b992c12a9b50227c05fc303442a49b28eb3e254
ewilson1 <ewilson1@fullsail.edu>
2015-04-23 15:25:59 -0400

    Height, width and square feet have been calculated
    Now, we are trying to add additional features such as lake view and
    golf view to the overall price per square foot to the home.

--- Wilson_Eric_Conditionals_Assignment/js/Conditionals_Assignment.js
+++ Wilson_Eric_Conditionals_Assignment/js/Conditionals_Assignment.js
@@ -11,4 +11,34 @@ alert("test to see if connected.")//They are connected
 
 */
 
- //
+ //We are going to see how much it costs per square foot to buy a home, with some added features and some without.
+
+
+ var length=prompt("Welcome! What is the length of your home in feet?");
+  console.log("Your home is " + length + " feet long.");
+
+ var width=prompt("Now, what is the width of your home in feet?");
+  console.log("Your home is " + width + " feet wide.");
+
+ var area = length * width;
+ alert("Your home is " + area + " square feet");
+ console.log("Your home is " + area + " square feet");
+
+ var lake=prompt("Does your home have a Lakeside view?\nYes or No.");
+ if(lake === "yes"){
+  console.log("Add $25,000 dollars to the value of your home.")
+ }else{
+  console.log("Does not have Lakeside view.")
+ }
+ var lakePrice = 25000;
+
+ var golf=prompt("Does your home have a golf course you can see from your property?\n Yes or No.");
+ if(golf === "yes"){
+  console.log("Add $50,000 dollars to the value of your home.")
+ }else{
+  console.log("Does not have a golf course view.")
+ }
+ var golfPrice = 50000;
+
+
+ //var homePrice=prompt("How much did you pay for your home?")
