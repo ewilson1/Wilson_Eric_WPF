fc01bb184ac862d195966cca9ed3b38da3c7fda7
ewilson1 <ewilson1@fullsail.edu>
2015-04-23 17:38:24 -0400

    Solving for views!!
    I used a long || equation to solve whether or not they have or don’t
    have a lake view or a golf view or both by using their declarations
    earlier I was allowed to use the number values to determine what they
    are going to pay extra in all 4 cases.
    1.No views of either, means no extra charge
    2. Golf, but no lake, means 25,000 extra charge
    3. Lake, but no golf, means 50,000 extra charge
    4. Both views, means 75,000 extra charge

--- Wilson_Eric_Conditionals_Assignment/js/Conditionals_Assignment.js
+++ Wilson_Eric_Conditionals_Assignment/js/Conditionals_Assignment.js
@@ -24,33 +24,36 @@ alert("test to see if connected.")//They are connected
  alert("Your home is " + area + " square feet");
  console.log("Your home is " + area + " square feet");
 
-//Time to add some features, such as a lake view. If they have one, it will add to the overall cost per square foot of the home.
- //first we must add value to our new variables and declare them true
-
- var lakePrice = 25000 && "yes";
- var golfPrice = 50000 && "yes";
+//Time to add some features, such as a lake view or a golf view.
 
  //then step through it with each luxury item
 
- var lake=prompt("Does your home have a Lakeside view?\nYes or No.");
+ var lake=prompt("Does your home have a Lakeside view?\nyes or no.");
  if(lake === "yes"){
-  lakePrice = 25000;
+  var lakePrice = 25000;
   console.log("Add $25,000 dollars to the value of your home.")
  }else{
+  var noLakeView = 0;
   console.log("Does not have Lakeside view.")
  }
 
-//same thing here, if they have a view of the golf course from thier home or not; as it will also add to the value of the home.
+//same thing here, just find out how much they paid in extras for the home. This will come up later in the code.
 
- var golf=prompt("Does your home have a golf course you can see from your property?\n Yes or No.");
+ var golf=prompt("Does your home have a golf course you can see from your property?\nyes or no.");
  if(golf === "yes"){
-  golfPrice = 50000;
+  var golfPrice = 50000;
   console.log("Add $50,000 dollars to the value of your home.")
  }else{
+  var noGolfView = 0;
   console.log("Does not have a golf course view.")
  }
 
 //I took out the complicated yes or no for lake and golf, took up way too much code and didn't run properly
+ //time to move on, if we need to know if they have one or the other we can come back to it later.
+ //we are simply trying to find out how much they paid per square foot of their home, not if they are going to buy it or not-which in that case we would need to know how much they were going to pay.
 
+ var homePrice=prompt("How much did you pay for your home?");
+ console.log("Your home cost $" +homePrice+ " dollars");
 
- //var homePrice=prompt("How much did you pay for your home?")
+ var luxuryItems= (lakePrice || noLakeView) || (golfPrice || noGolfView);
+  console.log("Your home has $" + luxuryItems + " dollars in luxury items.");
