a90712718eb26026abdb66eb3575e0227a5280c9
ewilson1 <ewilson1@fullsail.edu>
2015-04-27 15:42:25 -0400

    Switching the arguments after the parameters
    It doesn’t matter if the arguments come before or after the parameter
    with this function (only the anonymous ones do matter)

--- Functions Worksheet/js/Functions_Worksheet.js
+++ Functions Worksheet/js/Functions_Worksheet.js
@@ -50,14 +50,18 @@ alert("test to see if they are connected.");
 
  //It takes 8.67 bee stings per pound to kill an animal. Calculate how many stings are needed to kill an animal of a certain weight in pounds.
 
- var total = killAnimal(8.67, 244);//this is the function call or invoking from the arguments to the parameters. Now set up the parameters in the function killAnimal
 
- function killAnimal(s, p){//This is passing the arguments to the parameters s for sting and p for pounds.
+
+ function killAnimal(s, p){//This is setting up the parameters s for sting and p for pounds.
   var stings = s
   var pounds = p
   var kill = s * p;
   return kill;//this spits the information out the the var total in the function call
  }
+
+ var total = killAnimal(8.67, 244);//this is the function call or invoking from the arguments to the parameters. Now set up the parameters in the function killAnimal
+ //This time I moved the call or invocation from before to after the parameters were set in place.
+ //It still worked. So, Functions don't care if the invocation is before or after-that only matter with anonymous functions.
 
  console.log("It takes " +total.toFixed(2)+ " bee stings to kill this animal.");//Now play around with the weight and double check with a calculator to see if this is true.\
  //It works out to be true.
