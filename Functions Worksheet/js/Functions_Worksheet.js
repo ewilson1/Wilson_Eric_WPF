6cf47283cdb268503e621f0813a5365f9e5de6f9
ewilson1 <ewilson1@fullsail.edu>
2015-04-27 14:30:56 -0400

    Functions and Returns with English behind the lines of code
    Functions and Returns with English behind the lines of code

--- Functions Worksheet/js/Functions_Worksheet.js
+++ Functions Worksheet/js/Functions_Worksheet.js
@@ -16,11 +16,15 @@ alert("test to see if they are connected.");
 
 //Here is an example that actually works. The example from the instructions don't.
 
+//You can have the function before or after the declared variable "Total = calcArea"
 
- function calcArea(w, h){
- var area = w * h;
- return area
+ //Arguments and Parameters
+
+ //
+ function calcArea(w, h){//function functionName--in this case calcArea---(argument 1, argument 2)--in this case w, and h
+  var area = w * h;//passes the arguments, w and h,  (variables or givens) to the parameters
+ return area//Returns information to the function call---in this case var total
  }
- var total = calcArea(30, 20);
- console.log(total);
+ var total = calcArea(30, 20);//function call
+ console.log(total);//print out the result to the console to see if it's working.
 
