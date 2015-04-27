2eb7260d958fcc077df4f60294ba1777bcac2c59
ewilson1 <ewilson1@fullsail.edu>
2015-04-27 09:47:16 -0400

    Setting up the Functions Worksheet
    Setting up the Functions Worksheet

---
+++ Functions Worksheet/js/Functions_Worksheet.js
@@ -0,0 +1,17 @@
+
+ /*
+Eric Wilson
+SDI Section 01
+Title
+Date
+
+ 
+
+
+alert("test to see if they are connected.");
+ console.log("Test to see if they are connected");
+ */
+
+//They are connected
+
+
