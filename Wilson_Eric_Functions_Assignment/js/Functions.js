20a45a2a2d01ca73bea66c18e2e46fbad8d364df
ewilson1 <ewilson1@fullsail.edu>
2015-04-28 16:58:35 -0400

    First Room Complete, attempt at setting up anonymous function
    With the first room done, I have commented on what might work as an
    anonymous function. If it works, I will commit it later

--- Wilson_Eric_Functions_Assignment/js/Functions.js
+++ Wilson_Eric_Functions_Assignment/js/Functions.js
@@ -17,7 +17,8 @@ alert("Test to see if they are connected.");
 //Build a calculator. You need 1 Normal function, and 1 Anonymous function which must use arguments and parameters. Each function must contain at least 3 parameters. A value must be returned for each function and saved in a variable in the main code. Must pass arguments to the parameters. All user prompts must be validated using a while loop.
 
  alert("Welcome to Florida, it gets steamy here!\nLet's find out how many air conditioners you need to cool your home.\n-----\nTake notice: each air conditioner can effectively cool 5,000 cubic feet per house.");
-//Set up the total value for each air conditioner as a variable
+//Set up the total value for each air conditioner as a variable for the main code. The main code is saved for after we have all three variables from the user's input.
+
 var airCond = 5000;
 
 //Make sure to validate the prompts with a while loop
@@ -58,3 +59,18 @@ var airCond = 5000;
  //Now, print out the results of the living room and see how much room you have for extra rooms.
  console.log("Your living room is " +totalLiv+ " cubic feet.");
  console.log("You have " +(airCond - totalLiv)+ " cubic feet left.");
+
+ // We need to keep the result from the last calculation to refer to in the following room calculations. Could this be where we enter our anonymous function? Let's try it and see later in the program.
+
+ /*
+ var totalAirConditioner = function(areaLiv, areaRoom1, areaRoom2, areaRoom3, areaBath1, areaBath2, areaGarage) {
+  var totalArea = (totalLiv + totalRoom1 + totalRoom2 + totalRoom3 + totalBath1 + totalBath2 + totalGarage);
+  return totalArea;
+ }*/
+
+// -------
+ //Now repeat for each room in the home. If they come to a question regarding the amount of rooms-this could be where we place an anonymous function into the code-such as 'no', we can continue to the next set of questions such as a garage or Florida room. If they have neither, or they don't want to condition the air in the following rooms we will know how many air conditioners they need at the end.
+// -------
+
+
+
