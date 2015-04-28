e65a353b20e6ffa1d030a4ea249d1ab7083998af
ewilson1 <ewilson1@fullsail.edu>
2015-04-28 16:18:05 -0400

    Living room argument is complete
    Since we have more than one room in a house, this could get long. Cut
    and paste will help out a lot.
    Still need to work on an anonymous function.

--- Wilson_Eric_Functions_Assignment/js/Functions.js
+++ Wilson_Eric_Functions_Assignment/js/Functions.js
@@ -17,16 +17,44 @@ alert("Test to see if they are connected.");
 //Build a calculator. You need 1 Normal function, and 1 Anonymous function which must use arguments and parameters. Each function must contain at least 3 parameters. A value must be returned for each function and saved in a variable in the main code. Must pass arguments to the parameters. All user prompts must be validated using a while loop.
 
  alert("Welcome to Florida, it gets steamy here!\nLet's find out how many air conditioners you need to cool your home.\n-----\nTake notice: each air conditioner can effectively cool 5,000 cubic feet per house.");
+//Set up the total value for each air conditioner as a variable
+var airCond = 5000;
 
- var livArray = [livLength, livWidth, livHeight];
-
+//Make sure to validate the prompts with a while loop
  var livLength=prompt("Let's start with the length of your living room in feet?");
-while (livLength==="" || isNaN(livLength)) {
+ while (livLength==="" || isNaN(livLength)) {
  livLength = prompt("Please do not leave blank and only type in numbers.\nWhat is length of your living room in feet?");
- console.log("Your living room is " + livLength + " feet long.");
 }
- var livWidth=prompt("Let's start with the length of your living room in feet?");
+ //Find out what the length of the first room, the living room, is in cubic feet.
+ console.log("Your living room is " + livLength + " feet long.");
+
+ //Validate with a while loop.
+ var livWidth=prompt("Now, what is the width of your living room in feet?");
  while (livWidth==="" || isNaN(livWidth)){
-  livWidth = prompt("Please do not leave blank and only type in numbers.\nWhat is length of your living room in feet?");
-  console.log("Your living room is " +livWidth+ " feet wide.");
+  livWidth = prompt("Please do not leave blank and only type in numbers.\nWhat is width of your living room in feet?");
  }
+ //Now we have the width of the living room in cubic feet.
+ console.log("Your living room is " +livWidth+ " feet wide.");
+
+ //Validate with a while loop
+ var livHeight=prompt("Finally, what is the height of your living room in feet?");
+ while (livHeight==="" || isNaN(livHeight)) {
+  livHeight = prompt("Please do not leave blank and only type in numbers.\nWhat is height of your living room in feet?");
+ }
+ //Finally we have the last integer to find the cubic feet of the Living room.
+ console.log("Your living room is " + livHeight + " feet high.");
+
+ //Set the Living room function call
+
+ var totalLiv = calcLivRoom (livLength, livWidth, livHeight);
+
+ //now pass the arguments to the parameters below
+ function calcLivRoom (l,w,h){
+  areaLiv = l * w * h;
+  return areaLiv;
+ }
+//the return, spits the information out to the var totalLiv, and is now ready to perform additional calculations.
+
+ //Now, print out the results of the living room and see how much room you have for extra rooms.
+ console.log("Your living room is " +totalLiv+ " cubic feet.");
+ console.log("You have " +(airCond - totalLiv)+ " cubic feet left.");
